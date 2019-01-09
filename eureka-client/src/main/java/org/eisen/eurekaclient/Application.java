a80f2b055afe65cfa7a3ff8df9eff697ee07d5d4
Eisen <eisen7@yeah.net>
2019-01-09 17:30:06 +0800

    firest commit

---
+++ eureka-client/src/main/java/org/eisen/eurekaclient/Application.java
@@ -0,0 +1,13 @@
+package org.eisen.eurekaclient;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).run(args);
+    }
+}
